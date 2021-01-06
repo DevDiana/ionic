2778b9203843beecd9d78726bbf60d6ff224b053
DianaR96 <diana.regina@timeenergy.com.br>
2021-01-06 11:34:32 -0300

    enviando alteraçoes

--- myApp/src/app/app-routing.module.ts
+++ myApp/src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'navegacao',
+    loadChildren: () => import('./navegacao/navegacao.module').then( m => m.NavegacaoPageModule)
   }
 ];
 @NgModule({
